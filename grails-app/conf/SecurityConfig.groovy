e9e40f70d75e1fa0a6ab527be65a368eec041e38
Aurélien Maury <amaury@xebia.fr>
2010-07-21 12:52:33 +0200

    Comments in security config

--- grails-app/conf/SecurityConfig.groovy
+++ grails-app/conf/SecurityConfig.groovy
@@ -7,4 +7,10 @@ security {
 	loginUserDomainClass = "fr.xebia.nothunes.security.User"
 	authorityDomainClass = "fr.xebia.nothunes.security.Role"
 	requestMapClass = "fr.xebia.nothunes.security.RequestMap"
+
+	//useMail                 = true          // specifie d'envoyer un mail à l'enregistrement d'un compte
+        //mailHost                = 'localhost'   // serveur d'envoi de mails
+        //mailProtocol            = 'smtp'        // nom du protocole JavaMail
+        //mailFrom                = 'no.reply@bookstore.fr'   // adresse de l'envoyeur du mail
+        //mailPort                = 25            // port de connexion au serveur de mail	
 }
